7812789eb17692230a16542c9517d266a8d1c449
siobe34 <siobe14@hotmail.com>
2023-11-16 16:37:37 -0500

    replaced boilerplate router with routers relevant to db

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
-import { postRouter } from "@/server/api/routers/post";
+import { boardRouter } from "@/server/api/routers/board";
+import { challengeRouter } from "@/server/api/routers/challengeRouter";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -7,7 +8,8 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  board: boardRouter,
+  challenge: challengeRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/challengeRouter.ts
@@ -0,0 +1,5 @@
+import { createTRPCRouter } from "@/server/api/trpc";
+
+export const challengeRouter = createTRPCRouter({
+  //
+});
