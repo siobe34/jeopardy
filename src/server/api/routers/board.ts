fe306c5c34c4c20b8b46968f471ead3646eeb696
siobe34 <siobe14@hotmail.com>
2023-11-16 22:09:50 -0500

    fetch all jeopardy board of the current user

--- src/server/api/routers/board.ts
+++ src/server/api/routers/board.ts
@@ -1,5 +1,16 @@
-import { createTRPCRouter } from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 
 export const boardRouter = createTRPCRouter({
-  //
+  // create: protectedProcedure.input({}).mutation(({ ctx, input }) => {
+  //   //
+  // }),
+  getByCurrentUser: protectedProcedure.query(async ({ ctx }) => {
+    const currentUserId = ctx.session.userId;
+
+    const boards = await ctx.db.query.boards.findMany({
+      where: (boards, { eq }) => eq(boards.userId, currentUserId),
+    });
+
+    return boards;
+  }),
 });
