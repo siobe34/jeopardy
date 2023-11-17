96f67e8933779c54cd479d1b37a0ab8307210983
siobe34 <siobe14@hotmail.com>
2023-11-16 22:11:35 -0500

    bg gradient styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <SignedIn>
-        <SignOutButton>Sign out &rarr;</SignOutButton>
-      </SignedIn>
+    <main className="to-secondary flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[hsl(331,74.5%,65.4%)] to-70% text-white">
       <SignedOut>
         <SignIn
           afterSignInUrl={"/dashboard"}
