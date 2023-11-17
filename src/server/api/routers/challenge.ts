941079db2db54de89283084c5a88be0933629561
siobe34 <siobe14@hotmail.com>
2023-11-17 18:27:11 -0500

    method to query all jeopardy questoins for a particular board

--- src/server/api/routers/challenge.ts
+++ src/server/api/routers/challenge.ts
@@ -1,5 +1,16 @@
-import { createTRPCRouter } from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 
 export const challengeRouter = createTRPCRouter({
-  //
+  getByBoardAndUser: protectedProcedure
+    .input(z.object({ boardId: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const currentUserId = ctx.session.userId;
+
+      const queriedChallenges = await ctx.db.query.challenges.findMany({
+        with: { board: { columns: { userId: true } } },
+        where: (table, { eq }) => eq(table.boardId, input.boardId),
+      });
+
+      return queriedChallenges.filter((row) => row.board?.userId === currentUserId);
+    }),
 });
