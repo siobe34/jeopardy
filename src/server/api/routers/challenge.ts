190d9636f58e28203ed7f186f27fe93e50a1c4cf
siobe34 <siobe14@hotmail.com>
2023-11-17 18:27:35 -0500

    method to create new jeopardy questions for a particular board

--- src/server/api/routers/challenge.ts
+++ src/server/api/routers/challenge.ts
@@ -1,6 +1,25 @@
+import { z } from "zod";
+
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { challenges } from "@/server/db/schema";
+import { challengeSchema } from "@/types/challengeSchema";
 
 export const challengeRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(challengeSchema)
+    .mutation(async ({ ctx, input }) => {
+      const addedChallenges = await ctx.db.insert(challenges).values(
+        input.map((newChallenge) => ({
+          boardId: newChallenge.boardId,
+          question: newChallenge.question,
+          answer: newChallenge.answer,
+          category: newChallenge.category,
+          points: newChallenge.points,
+        })),
+      );
+
+      return addedChallenges;
+    }),
   getByBoardAndUser: protectedProcedure
     .input(z.object({ boardId: z.number() }))
     .query(async ({ ctx, input }) => {
@@ -11,6 +30,8 @@ export const challengeRouter = createTRPCRouter({
         where: (table, { eq }) => eq(table.boardId, input.boardId),
       });
 
-      return queriedChallenges.filter((row) => row.board?.userId === currentUserId);
+      return queriedChallenges.filter(
+        (row) => row.board?.userId === currentUserId,
+      );
     }),
 });
