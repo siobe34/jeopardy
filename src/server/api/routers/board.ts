59606f34870a7f9536f627c7b317c9e671875ac9
siobe34 <siobe14@hotmail.com>
2023-11-16 22:56:49 -0500

    method to create a new board

--- src/server/api/routers/board.ts
+++ src/server/api/routers/board.ts
@@ -1,9 +1,22 @@
+import { z } from "zod";
+
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { boards } from "@/server/db/schema";
 
 export const boardRouter = createTRPCRouter({
-  // create: protectedProcedure.input({}).mutation(({ ctx, input }) => {
-  //   //
-  // }),
+  create: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const currentUserId = ctx.session.userId;
+
+      const newBoard = await ctx.db.insert(boards).values({
+        name: input.name,
+        userId: currentUserId,
+      });
+
+      // TODO: fix return, currently returns an ExecutedQuery
+      return newBoard;
+    }),
   getByCurrentUser: protectedProcedure.query(async ({ ctx }) => {
     const currentUserId = ctx.session.userId;
 
