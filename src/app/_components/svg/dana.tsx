377bbb478122f7d873fe590fac8b7e72def57b30
siobe34 <siobe14@hotmail.com>
2023-11-18 15:12:36 -0500

    svg for "hbd dana"

---
+++ src/app/_components/svg/dana.tsx
@@ -0,0 +1,70 @@
+export const Dana = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="svg8"
+    version="1.1"
+    viewBox="0 0 64 32"
+    height="32mm"
+    width="64mm"
+    className={className}
+  >
+    <defs id="defs2">
+      <linearGradient id="linearGradient2302">
+        <stop
+          id="stop2298"
+          offset="0"
+          style={{ stopColor: "#ed84b7", stopOpacity: 1 }}
+        />
+        <stop
+          id="stop2300"
+          offset="1"
+          style={{ stopColor: "#ed84b7", stopOpacity: 0 }}
+        />
+      </linearGradient>
+    </defs>
+    <g transform="translate(0,-265)" id="layer1">
+      <g
+        id="flowRoot815"
+        // style="font-style:normal;font-weight:normal;font-size:40px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ed84b7;fill-opacity:1;stroke:#ed84b7;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+        transform="matrix(0.32175087,0,0,0.36525974,-36.192658,156.27836)"
+        aria-label="Hbd Dana"
+      >
+        <path
+          id="path823"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 118.70258,357.4868 v -0.4 h 0.52 q 3.28,0 3.28,-2.88 v -22.16 q 0,-2.72 -3.28,-2.72 h -0.52 v -0.4 h 11.64 v 0.4 h -0.52 q -3.28,0 -3.28,2.88 v 10.16 h 12.96 v -10.16 q 0,-2.88 -3.28,-2.88 h -0.52 v -0.4 h 11.64 v 0.4 h -0.52 q -3.28,0 -3.28,2.72 v 22.16 q 0,2.88 3.28,2.88 h 0.52 v 0.4 h -11.64 v -0.4 h 0.52 q 3.28,0 3.28,-2.88 v -11.44 h -12.96 v 11.44 q 0,2.88 3.28,2.88 h 0.52 v 0.4 z"
+        />
+        <path
+          id="path825"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 163.18255,357.8868 q -2.6,0 -4.12,-1.48 -1.52,-1.48 -2.08,-4.56 h -0.08 v 5.64 h -7.32 v -0.4 h 0.32 q 1.76,0 2.52,-0.72 0.76,-0.72 0.76,-2.36 v -23.4 q 0,-1.68 -0.76,-2.4 -0.76,-0.72 -2.52,-0.72 h -0.12 v -0.4 h 7.16 v 7.36 q 0,1.04 0,3.04 0,1.96 -0.08,4.12 h 0.08 q 0.56,-3.04 2.08,-4.48 1.56,-1.48 4.24,-1.48 7.96,0 7.96,11.08 0,5.48 -2.08,8.32 -2.04,2.84 -5.96,2.84 z m -0.68,-0.6 q 2.6,0 3.72,-2.44 1.12,-2.44 1.12,-8.16 0,-5.64 -1.12,-8.04 -1.12,-2.4 -3.76,-2.4 -2.92,0 -4.24,2.64 -1.28,2.64 -1.28,7.84 0,5.36 1.36,7.96 1.4,2.6 4.2,2.6 z"
+        />
+        <path
+          id="path827"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 183.66249,357.8868 q -3.92,0 -6,-2.84 -2.04,-2.84 -2.04,-8.32 0,-11.08 7.96,-11.08 2.72,0 4.24,1.48 1.56,1.44 2.08,4.48 h 0.08 q -0.04,-2.16 -0.08,-4.12 0,-2 0,-3.04 v -3.84 q 0,-1.68 -0.76,-2.4 -0.76,-0.72 -2.52,-0.72 h -0.12 v -0.4 h 7.16 v 26.92 q 0,1.64 0.76,2.36 0.76,0.72 2.52,0.72 h 0.32 v 0.4 h -7.32 v -5.64 h -0.08 q -0.52,3.08 -2.08,4.56 -1.52,1.48 -4.12,1.48 z m 0.68,-0.6 q 2.84,0 4.2,-2.6 1.36,-2.6 1.36,-7.96 0,-5.2 -1.32,-7.84 -1.28,-2.64 -4.2,-2.64 -2.64,0 -3.76,2.4 -1.12,2.4 -1.12,8.04 0,5.72 1.12,8.16 1.12,2.44 3.72,2.44 z"
+        />
+        <path
+          id="path829"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 208.50236,357.4868 v -0.4 h 0.52 q 3.28,0 3.28,-2.88 v -22.16 q 0,-2.72 -3.28,-2.72 h -0.52 v -0.4 h 12.24 q 6.2,0 9.6,3.72 3.44,3.72 3.44,10.52 0,4.36 -1.48,7.6 -1.44,3.2 -4.32,4.96 -2.88,1.76 -7.24,1.76 z m 7.84,-0.4 h 3.48 q 5,0 7.24,-3.28 2.28,-3.32 2.28,-10.64 0,-7.28 -2.24,-10.56 -2.24,-3.28 -7.24,-3.28 h -3.52 z"
+        />
+        <path
+          id="path831"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 244.66233,357.8868 q -3.04,0 -4.84,-1.56 -1.76,-1.6 -1.76,-4.36 0,-3.08 2.24,-4.6 2.28,-1.52 6.88,-1.68 l 3.32,-0.12 v -3 q 0,-2.24 -0.24,-3.68 -0.24,-1.44 -1.04,-2.12 -0.8,-0.72 -2.44,-0.72 -2.32,0 -3,1.44 -0.64,1.44 -0.64,4.52 -1.68,0 -2.56,-0.56 -0.84,-0.56 -0.84,-1.84 0,-1.44 0.96,-2.28 1,-0.88 2.64,-1.28 1.64,-0.4 3.6,-0.4 3.68,0 5.48,1.56 1.84,1.56 1.84,5.36 v 11.96 q 0,1.44 0.56,2 0.56,0.56 1.92,0.56 h 0.12 v 0.4 h -6.04 v -4.2 h -0.08 q -0.64,2.4 -2.28,3.52 -1.6,1.08 -3.8,1.08 z m 0.8,-0.56 q 1.48,0 2.6,-0.76 1.16,-0.76 1.8,-2.04 0.64,-1.28 0.64,-2.76 v -5.76 l -2.56,0.12 q -3.4,0.16 -4.72,1.56 -1.28,1.4 -1.28,4.72 0,4.92 3.52,4.92 z"
+        />
+        <path
+          id="path833"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 259.70232,357.4868 v -0.4 h 0.32 q 1.88,0 2.56,-0.8 0.72,-0.8 0.72,-2.88 v -13.36 q 0,-2.04 -0.68,-2.8 -0.68,-0.8 -2.52,-0.8 h -0.2 v -0.4 h 7.12 v 3.76 h 0.08 q 0.56,-1.88 1.56,-2.76 1,-0.88 2.16,-1.12 1.2,-0.28 2.2,-0.28 3.56,0 5.28,1.88 1.76,1.84 1.76,5.96 v 9.92 q 0,2.08 0.64,2.88 0.64,0.8 2.36,0.8 h 0.2 v 0.4 h -6.96 v -15.36 q 0,-2.6 -0.96,-4.24 -0.96,-1.64 -3.32,-1.64 -1.88,0 -2.96,1 -1.08,1 -1.56,2.6 -0.44,1.56 -0.44,3.28 v 10.28 q 0,2.08 0.68,2.88 0.68,0.8 2.52,0.8 h 0.2 v 0.4 z"
+        />
+        <path
+          id="path835"
+          // style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Noto Serif Display';-inkscape-font-specification:'Noto Serif Display';fill:#ed84b7;fill-opacity:1;stroke-width:0.87510565;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ed84b7;stroke-opacity:1"
+          d="m 292.98226,357.8868 q -3.04,0 -4.84,-1.56 -1.76,-1.6 -1.76,-4.36 0,-3.08 2.24,-4.6 2.28,-1.52 6.88,-1.68 l 3.32,-0.12 v -3 q 0,-2.24 -0.24,-3.68 -0.24,-1.44 -1.04,-2.12 -0.8,-0.72 -2.44,-0.72 -2.32,0 -3,1.44 -0.64,1.44 -0.64,4.52 -1.68,0 -2.56,-0.56 -0.84,-0.56 -0.84,-1.84 0,-1.44 0.96,-2.28 1,-0.88 2.64,-1.28 1.64,-0.4 3.6,-0.4 3.68,0 5.48,1.56 1.84,1.56 1.84,5.36 v 11.96 q 0,1.44 0.56,2 0.56,0.56 1.92,0.56 h 0.12 v 0.4 h -6.04 v -4.2 h -0.08 q -0.64,2.4 -2.28,3.52 -1.6,1.08 -3.8,1.08 z m 0.8,-0.56 q 1.48,0 2.6,-0.76 1.16,-0.76 1.8,-2.04 0.64,-1.28 0.64,-2.76 v -5.76 l -2.56,0.12 q -3.4,0.16 -4.72,1.56 -1.28,1.4 -1.28,4.72 0,4.92 3.52,4.92 z"
+        />
+      </g>
+    </g>
+  </svg>
+);
