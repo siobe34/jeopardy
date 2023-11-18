e1f83bc28c5b6f1b9ed3ffce5fe1e1c01bb65847
siobe34 <siobe14@hotmail.com>
2023-11-17 19:08:41 -0500

    constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_JEOPARDY_BOARD_STATE = {
+  question1: { question: "", answer: "", points: 100 },
+  question2: { question: "", answer: "", points: 200 },
+  question3: { question: "", answer: "", points: 300 },
+  question4: { question: "", answer: "", points: 400 },
+  question5: { question: "", answer: "", points: 500 },
+};
+
+export const DEFAULT_JEOPARDY_CATEGORY_STATE = "";
+
+export const DEFAULT_JEOPARDY_BUTTON_STATE = false;
