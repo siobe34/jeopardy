9c170eaf9a9f54072615a0c494abd559e850e666
siobe34 <siobe14@hotmail.com>
2023-11-18 12:58:49 -0500

    zod schema to parse sign in fields

---
+++ src/types/signInSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: "You did not enter a valid email address." }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
