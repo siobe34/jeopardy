de15af6dbbb84cb0bf3073d3525c683f78528738
siobe34 <siobe14@hotmail.com>
2023-11-18 21:38:22 -0500

    method to add points to a team

--- src/server/api/routers/teams.ts
+++ src/server/api/routers/teams.ts
@@ -1,3 +1,5 @@
+import { TRPCError } from "@trpc/server";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
@@ -16,6 +18,37 @@ export const teamRouter = createTRPCRouter({
       );
 
       return addedTeams;
+    }),
+  addPointsByTeam: publicProcedure
+    .input(
+      z.object({ boardId: z.number(), name: z.string(), points: z.number() }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const teamToUpdate = await ctx.db.query.teams.findFirst({
+        where: (table, { eq }) =>
+          and(eq(table.boardId, input.boardId), eq(table.name, input.name)),
+      });
+
+      if (!teamToUpdate) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Could not find a team to update points for.",
+        });
+      }
+
+      const newScore = teamToUpdate.score + input.points;
+
+      await ctx.db
+        .update(teams)
+        .set({ score: newScore })
+        .where(
+          and(
+            eq(teams.boardId, input.boardId),
+            eq(teams.name, teamToUpdate.name),
+          ),
+        );
+
+      return teamToUpdate;
     }),
   getByBoard: publicProcedure
     .input(z.object({ boardId: z.number() }))
