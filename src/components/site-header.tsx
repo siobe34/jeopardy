f72068b925878c838be4a8c1b0edee52374c86b4
ibad <siobe14@hotmail.com>
2024-05-24 14:42:30 -0400

    use site routes constant for href

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,10 +1,12 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
+import { SITE_ROUTES } from "@/lib/site-routes";
+
 export const SiteHeader = () => {
   return (
     <header className="flex items-center justify-between gap-2 px-2">
-      <Link href="/">Jeopardy Logo</Link>
+      <Link href={SITE_ROUTES.home.path}>Jeopardy Logo</Link>
       <SignedIn>
         <UserButton />
       </SignedIn>
