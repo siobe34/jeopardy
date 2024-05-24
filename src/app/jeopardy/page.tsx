70a619788c39590e46420eba96bd30dc4c21904b
ibad <siobe14@hotmail.com>
2024-05-24 11:15:43 -0400

    root path for jeopardy game

---
+++ src/app/jeopardy/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Jeopardy</h1>
+      <section className="bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        the Jeopardy dashboard goes here
+      </section>
+    </>
+  );
+}
