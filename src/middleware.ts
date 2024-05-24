9c2dd45f265220d2cb6e85b6024af8dc90ae1f65
ibad <siobe14@hotmail.com>
2024-05-23 23:21:38 -0400

    auth middleware with clerk

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// TODO: add protected routes here (regex patterns supported)
+const isProtectedRoute = createRouteMatcher([]);
+
+export default clerkMiddleware((auth, req) => {
+  // Redirect all unauthenticated requests to protected routes to the login page
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
