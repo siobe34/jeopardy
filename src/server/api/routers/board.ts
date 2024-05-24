61c9f6e82e2dedda5113442577ef2f23ff3fab87
ibad <siobe14@hotmail.com>
2024-05-24 09:26:10 -0400

    method to get all boards by a user

--- src/server/api/routers/board.ts
+++ src/server/api/routers/board.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
 import { boards } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
 
 const createBoardInput = z.object({
   name: z.string().max(128, {
@@ -24,4 +25,17 @@ export const boardRouter = createTRPCRouter({
 
       return newBoard[0];
     }),
+  getByUser: privateProcedure.query(async ({ ctx }) => {
+    const usersJeopardyBoards = await ctx.db
+      .select({
+        name: boards.name,
+        userId: boards.userId,
+        createdAt: boards.createdAt,
+        updatedAt: boards.updatedAt,
+      })
+      .from(boards)
+      .where(eq(boards.userId, ctx.userId));
+
+    return usersJeopardyBoards;
+  }),
 });
