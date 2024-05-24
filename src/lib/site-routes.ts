859986d760a487b1843514c0e33447e3f61eeb74
ibad <siobe14@hotmail.com>
2024-05-24 14:04:26 -0400

    constant to reference all site routes

---
+++ src/lib/site-routes.ts
@@ -0,0 +1,14 @@
+export const SITE_ROUTES = {
+  home: { path: "/", title: "Home", protected: false },
+  jeopardyDashboard: { path: "/jeopardy", title: "Jeopardy", protected: true },
+  jeopardyCreate: {
+    path: "/jeopardy/board/create",
+    title: "Create New Jeopardy",
+    protected: true,
+  },
+  jeopardyPlay: {
+    path: "/jeopardy/board/play",
+    title: "Play Jeopardy!",
+    protected: true,
+  },
+};
