ac6e91875d0528cbf30ba84621e9664a9c9682f6
ibad <siobe14@hotmail.com>
2024-05-24 11:17:30 -0400

    sign in from / or redirect if signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { SignInButton, SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { userId } = auth();
+
+  if (userId) redirect("/jeopardy");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    </main>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
   );
 }
-
