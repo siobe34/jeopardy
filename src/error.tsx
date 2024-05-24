bcd6537b60c2f242edb449b5545377b80fcf69b8
ibad <siobe14@hotmail.com>
2024-05-24 13:44:51 -0400

    root error boundary

---
+++ src/error.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
