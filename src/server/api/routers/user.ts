58d500aef4e2bfd74e16e19976a7145654b27724
ibad <siobe14@hotmail.com>
2024-05-23 22:56:37 -0400

    renamed template post router to user router

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { users } from "@/server/db/schema";
+
+export const userRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+});
