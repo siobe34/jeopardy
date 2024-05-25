b097e5535f25b52d2f3ac98d25e45d7d7a074f6f
ibad <siobe14@hotmail.com>
2024-05-25 11:55:27 -0400

    button to submit form and client side interactivity relative to form status

--- src/components/jeopardy-form/submit-button.tsx
+++ src/components/jeopardy-form/submit-button.tsx
@@ -2,12 +2,39 @@
 
 import { useFormStatus } from "react-dom";
 
-export function JeopardyFormSubmitButton() {
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { cn } from "@/lib/cn";
+
+export function JeopardyFormSubmitButton({
+  children,
+  className,
+  ...props
+}: ButtonProps) {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" disabled={pending}>
-      Add
-    </button>
+    <Button
+      type="submit"
+      className={cn(className, "relative")}
+      disabled={pending}
+      {...props}
+    >
+      <span
+        className={cn(
+          "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0",
+          pending && "opacity-100",
+        )}
+      >
+        . . .
+      </span>
+      <span
+        className={cn(
+          "opacity-100",
+          pending && "pointer-events-none opacity-0",
+        )}
+      >
+        {children}
+      </span>
+    </Button>
   );
 }
