54573d0a829c3c909cccda792e5d24082cb1114a
ibad <siobe14@hotmail.com>
2024-05-26 05:41:41 -0400

    styled container for playing jeopardy game

--- src/app/jeopardy/board/play/page.tsx
+++ src/app/jeopardy/board/play/page.tsx
@@ -1,4 +1,12 @@
-export default function Page() {
-  console.log();
-  return <>Play board page</>;
+import { type SearchParams } from "@/lib/global-types";
+
+export default function Page({ searchParams }: { searchParams: SearchParams }) {
+  console.log(searchParams.id);
+  return (
+    // max height of 95vh is very "magic numbery" and almost certainly breaks down in some cases
+    // oh and the 95vh is because the overall app grid is 5vh for the header and 95vh for the main content
+    <div className="grid h-full max-h-[95vh] w-full overflow-hidden border-8 border-fuchsia-600">
+      Board Id: {searchParams.id}
+    </div>
+  );
 }
