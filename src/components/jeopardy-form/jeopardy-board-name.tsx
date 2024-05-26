637af19698f578f9e097468ec23a6cf05eaa63b0
ibad <siobe14@hotmail.com>
2024-05-26 04:47:40 -0400

    change URL state on input change

--- src/components/jeopardy-form/jeopardy-board-name.tsx
+++ src/components/jeopardy-form/jeopardy-board-name.tsx
@@ -1,30 +1,47 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/cn";
+import { SITE_ROUTES } from "@/lib/site-routes";
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
 export const JeopardyBoardName = (props: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const initialValue = searchParams.get("name") ?? "New Board";
+
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    if (value === "") return;
+
+    router.replace(`${SITE_ROUTES.jeopardyCreate.path}?name=${value}`);
+  }, [value]);
+
   return (
     <div
       className={cn("flex flex-col-reverse gap-1", props.className)}
       {...props}
     >
+      <Label
+        htmlFor="boardName"
+        className="text-2xl font-semibold leading-loose tracking-tight"
+      >
+        Board Name
+      </Label>
       <Input
         id="boardName"
         name="boardName"
         type="text"
-        className="peer invalid:text-destructive invalid:focus-visible:ring-destructive dark:invalid:text-destructive-foreground dark:invalid:focus-visible:ring-destructive-foreground"
         placeholder=""
+        defaultValue={value}
+        onChange={({ target }) => setValue(target.value)}
       />
-      <Label
-        htmlFor="boardName"
-        className="duration-30 pointer-events-none text-foreground transition-all peer-placeholder-shown:translate-x-3 peer-placeholder-shown:translate-y-8 peer-placeholder-shown:text-foreground/80 peer-invalid:text-destructive peer-[:focus-within:placeholder-shown]:text-foreground/80 dark:peer-invalid:text-destructive-foreground"
-      >
-        Board Name
-      </Label>
     </div>
   );
 };
