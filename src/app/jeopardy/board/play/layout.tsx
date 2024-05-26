4ba8729d28507cd74183795bdaef4aa4710028c5
ibad <siobe14@hotmail.com>
2024-05-26 12:58:36 -0400

    nested layout for playing jeopardy route

---
+++ src/app/jeopardy/board/play/layout.tsx
@@ -0,0 +1,13 @@
+export default function JeopardyPlayLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    // max height of 95vh is very "magic numbery" and almost certainly breaks down in some cases
+    // the 95vh is because the overall app grid is 5vh for the header and 95vh for the main content
+    <div className="grid h-full max-h-[95vh] w-full overflow-hidden border-8 border-fuchsia-600">
+      {children}
+    </div>
+  );
+}
--- src/app/jeopardy/board/play/page.tsx
+++ src/app/jeopardy/board/play/page.tsx
@@ -25,11 +25,10 @@ export default async function Page({
   // make sure game id actually exists
 
   return (
-    // max height of 95vh is very "magic numbery" and almost certainly breaks down in some cases
-    // oh and the 95vh is because the overall app grid is 5vh for the header and 95vh for the main content
-    <div className="grid h-full max-h-[95vh] w-full overflow-hidden border-8 border-fuchsia-600">
-      Board Id: {searchParams.boardId}
-    </div>
+    <>
+      <span>Board Id: {boardId}</span>
+      <span>Game Id: {gameId}</span>
+    </>
   );
 }
 
