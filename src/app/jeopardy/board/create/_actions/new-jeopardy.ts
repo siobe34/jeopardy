d76023e2341f512172501d51c543656acaf30db9
ibad <siobe14@hotmail.com>
2024-05-26 00:58:53 -0400

    reminder to remove manual slow down of function

--- src/app/jeopardy/board/create/_actions/new-jeopardy.ts
+++ src/app/jeopardy/board/create/_actions/new-jeopardy.ts
@@ -4,6 +4,7 @@ export const createJeopardyBoard = async (
   prevState: { message: string },
   formData: FormData,
 ) => {
+  // TODO: remove manual throttling
   await new Promise((resolve) => setTimeout(resolve, 2000));
   console.log(formData.entries());
 
