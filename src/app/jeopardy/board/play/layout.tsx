69decdd8667a2a8a72ce63d08ef9a51d976e5792
ibad <siobe14@hotmail.com>
2024-05-26 19:59:35 -0400

    setting grid dimensions

--- src/app/jeopardy/board/play/layout.tsx
+++ src/app/jeopardy/board/play/layout.tsx
@@ -6,7 +6,7 @@ export default function JeopardyPlayLayout({
   return (
     // max height of 95vh is very "magic numbery" and almost certainly breaks down in some cases
     // the 95vh is because the overall app grid is 5vh for the header and 95vh for the main content
-    <div className="grid h-full max-h-[95vh] w-full overflow-hidden border-8 border-fuchsia-600">
+    <div className="grid h-full max-h-[95vh] w-full grid-rows-[auto_1fr] overflow-hidden border-8 border-fuchsia-600">
       {children}
     </div>
   );
