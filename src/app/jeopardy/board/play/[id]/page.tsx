a84a6e5cb5a609cb35fca3cd5cc7cc817ca3f33f
ibad <siobe14@hotmail.com>
2024-05-26 14:33:50 -0400

    getting the board challenges for a jeopardy by game and board id

--- src/app/jeopardy/board/play/[id]/page.tsx
+++ src/app/jeopardy/board/play/[id]/page.tsx
@@ -1,3 +1,28 @@
-export default function Page({ params }: { params: { id: string } }) {
+import { z } from "zod";
+
+import { api } from "@/trpc/server";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const gameId = parseParamAndGetGameId(params);
+
+  const { boardId } = await api.game.getById({ gameId });
+
+  // get the board challenges
+  const boardChallenges = await api.boardChallenges.getAllByBoard({ boardId });
+
   return <h1>{params.id}</h1>;
 }
+
+const parseParamAndGetGameId = (params: { id: string }) => {
+  const unsafeGameId = +params.id;
+
+  const zodParser = z.number().safeParse(unsafeGameId);
+
+  if (!zodParser.success) {
+    throw new Error(
+      "This game does not appear to exist. Please try creating a new game.",
+    );
+  }
+
+  return zodParser.data;
+};
