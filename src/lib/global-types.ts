12204720ff916dba75529c47e91244374c615323
ibad <siobe14@hotmail.com>
2024-05-26 13:24:34 -0400

    co-located types where they're used

--- src/lib/global-types.ts
+++ src/lib/global-types.ts
@@ -1,11 +1,1 @@
 export type SearchParams = Record<string, string | string[] | undefined>;
-
-export type FormState = {
-  responseType: "success" | "error" | null;
-  serverResponses: string[] | null;
-};
-
-export type JeopardyFormProps = {
-  children: React.ReactNode;
-  action: (prevState: FormState, formData: FormData) => Promise<FormState>;
-};
