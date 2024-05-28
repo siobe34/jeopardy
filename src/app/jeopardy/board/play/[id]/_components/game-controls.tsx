f54347a22097a9b611d61c146c7029b56e0d0bc0
ibad <siobe14@hotmail.com>
2024-05-27 23:20:05 -0400

    alert dialog to confirm resetting of game

--- src/app/jeopardy/board/play/[id]/_components/game-controls.tsx
+++ src/app/jeopardy/board/play/[id]/_components/game-controls.tsx
@@ -1,10 +1,6 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
+import { ResetGameButton } from "@/app/jeopardy/board/play/[id]/_components/reset-game-button";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { api } from "@/trpc/react";
 
 export const GameControls = ({
   gameId,
@@ -13,35 +9,9 @@ export const GameControls = ({
   gameId: number;
   boardId: number;
 }) => {
-  const router = useRouter();
-
-  const { mutate: resetTeamPoints, isPending: teamMutationPending } =
-    api.team.resetTeamPointsByGame.useMutation({
-      onSuccess: () => router.refresh(),
-    });
-
-  const { mutate: resetBoardChallenges, isPending: boardResetPending } =
-    api.boardChallenges.resetBoard.useMutation({
-      onSuccess: () => router.refresh(),
-    });
-
-  const isPending = teamMutationPending && boardResetPending;
-
-  const handleGameReset = () => {
-    resetTeamPoints({ gameId });
-    resetBoardChallenges({ boardId });
-  };
-
   return (
     <Card className="flex h-full flex-col items-center justify-around gap-2 p-4">
-      <Button
-        variant="ghost"
-        className="border border-transparent bg-accent hover:border-border"
-        onClick={handleGameReset}
-        disabled={isPending}
-      >
-        Reset Board
-      </Button>
+      <ResetGameButton gameId={gameId} boardId={boardId} />
       <Button
         variant="ghost"
         className="border border-transparent bg-accent hover:border-border"
---
+++ src/app/jeopardy/board/play/[id]/_components/reset-game-button.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/cn";
+import { api } from "@/trpc/react";
+
+export const ResetGameButton = ({
+  gameId,
+  boardId,
+}: {
+  gameId: number;
+  boardId: number;
+}) => {
+  const router = useRouter();
+
+  const { mutate: resetTeamPoints, isPending: teamMutationPending } =
+    api.team.resetTeamPointsByGame.useMutation({
+      onSuccess: () => router.refresh(),
+    });
+
+  const { mutate: resetBoardChallenges, isPending: boardResetPending } =
+    api.boardChallenges.resetBoard.useMutation({
+      onSuccess: () => router.refresh(),
+    });
+
+  const isPending = teamMutationPending && boardResetPending;
+
+  const handleGameReset = () => {
+    resetTeamPoints({ gameId });
+    resetBoardChallenges({ boardId });
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className="border border-transparent bg-accent hover:border-border"
+        >
+          Reset Game
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>This cannot be undone!!</AlertDialogTitle>
+          <AlertDialogDescription>
+            Resetting the game will set all questions to be unsolved and set all
+            teams to have 0 points.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className={cn(buttonVariants({ variant: "destructive" }))}
+            onClick={handleGameReset}
+            disabled={isPending}
+          >
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
