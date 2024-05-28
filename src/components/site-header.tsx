60b6807927ee6e98517126e87ae67b0d9dc2f9fe
ibad <siobe14@hotmail.com>
2024-05-28 00:08:23 -0400

    added logo

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,11 +2,14 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link, { type LinkProps } from "next/link";
 
 import { SITE_ROUTES } from "@/lib/site-routes";
+import { Logo } from "./logo";
 
 export const SiteHeader = () => {
   return (
     <header className="flex items-center justify-between gap-2 px-2">
-      <Link href={SITE_ROUTES.home.path}>Jeopardy Logo</Link>
+      <Link href={SITE_ROUTES.home.path}>
+        <Logo className="h-8 w-24" />
+      </Link>
       <div className="flex items-center justify-center gap-8">
         <SignedIn>
           <SiteHeaderLink href={SITE_ROUTES.jeopardyCreate.path}>
