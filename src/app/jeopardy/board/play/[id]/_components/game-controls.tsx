fa5662602fe6c5acb0be53c1b7fdc7c28577d426
ibad <siobe14@hotmail.com>
2024-05-27 23:09:23 -0400

    reset team points and board when reset game is clicked

--- src/app/jeopardy/board/play/[id]/_components/game-controls.tsx
+++ src/app/jeopardy/board/play/[id]/_components/game-controls.tsx
@@ -6,17 +6,30 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { api } from "@/trpc/react";
 
-export const GameControls = ({ boardId }: { boardId: number }) => {
+export const GameControls = ({
+  gameId,
+  boardId,
+}: {
+  gameId: number;
+  boardId: number;
+}) => {
   const router = useRouter();
 
-  const { mutate, isPending } = api.boardChallenges.resetBoard.useMutation({
-    onSuccess: () => {
-      router.refresh();
-    },
-  });
+  const { mutate: resetTeamPoints, isPending: teamMutationPending } =
+    api.team.resetTeamPointsByGame.useMutation({
+      onSuccess: () => router.refresh(),
+    });
+
+  const { mutate: resetBoardChallenges, isPending: boardResetPending } =
+    api.boardChallenges.resetBoard.useMutation({
+      onSuccess: () => router.refresh(),
+    });
+
+  const isPending = teamMutationPending && boardResetPending;
 
   const handleGameReset = () => {
-    mutate({ boardId });
+    resetTeamPoints({ gameId });
+    resetBoardChallenges({ boardId });
   };
 
   return (
