318c69e8682d29a3498549a39723a1048e4e6f3f
ibad <siobe14@hotmail.com>
2024-06-04 01:06:11 -0400

    site header styling for mobile

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -7,11 +7,11 @@ import { SITE_ROUTES } from "@/lib/site-routes";
 
 export const SiteHeader = () => {
   return (
-    <header className="mb-3 flex items-center justify-between gap-2 bg-background px-2 pb-3 pt-2 shadow-md">
+    <header className="relative mb-40 flex items-center justify-between gap-2 bg-background px-2 pb-3 pt-2 shadow-md sm:mb-3">
       <Link href={SITE_ROUTES.home.path}>
         <Logo className="h-8 w-24 fill-foreground" />
       </Link>
-      <div className="flex items-center justify-center gap-8">
+      <div className="absolute left-1/2 top-full flex w-full -translate-x-1/2 flex-col items-center justify-center border-b-2 bg-background sm:static sm:ml-auto sm:w-fit sm:translate-x-0 sm:flex-row sm:gap-8 sm:border-b-0 sm:pr-2">
         <SignedIn>
           <SiteHeaderLink href={SITE_ROUTES.jeopardyCreate.path}>
             Create New Board
@@ -19,13 +19,15 @@ export const SiteHeader = () => {
           <SiteHeaderLink href={SITE_ROUTES.jeopardyDashboard.path}>
             All Boards
           </SiteHeaderLink>
-          <ThemeToggler />
-          <UserButton />
         </SignedIn>
-        <SignedOut>
-          <ThemeToggler />
-        </SignedOut>
       </div>
+      <SignedIn>
+        <ThemeToggler className="ml-auto mr-2 sm:ml-2" />
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <ThemeToggler />
+      </SignedOut>
     </header>
   );
 };
@@ -38,7 +40,7 @@ const SiteHeaderLink = ({ children, ...props }: SiteHeaderLinkProps) => {
   return (
     <Link
       {...props}
-      className="rounded-md px-4 py-2 text-foreground/80 transition-all hover:bg-accent hover:text-foreground"
+      className="w-full rounded-md border-t px-4 py-6 text-center text-foreground/80 transition-all hover:bg-accent hover:text-foreground sm:w-auto sm:border-t-0 sm:py-2"
     >
       {children}
     </Link>
