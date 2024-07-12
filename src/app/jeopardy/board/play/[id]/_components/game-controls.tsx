3a2c20cca6fc06967707bcc6afc184555c23a5e9
ibad <siobe14@hotmail.com>
2024-07-12 12:21:45 -0400

    added game settings functionality

--- src/app/jeopardy/board/play/[id]/_components/game-controls.tsx
+++ src/app/jeopardy/board/play/[id]/_components/game-controls.tsx
@@ -1,23 +1,21 @@
 import { ResetGameButton } from "@/app/jeopardy/board/play/[id]/_components/reset-game-button";
-import { Button } from "@/components/ui/button";
+import { SettingsButton } from "@/app/jeopardy/board/play/[id]/_components/settings-button";
 import { Card } from "@/components/ui/card";
+import { api } from "@/trpc/server";
 
-export const GameControls = ({
+export const GameControls = async ({
   gameId,
   boardId,
 }: {
   gameId: number;
   boardId: number;
 }) => {
+  const teams = await api.team.getByGame({ gameId });
+
   return (
     <Card className="flex h-full flex-col items-center justify-around gap-2 p-4">
       <ResetGameButton gameId={gameId} boardId={boardId} />
-      <Button
-        variant="secondary"
-        disabled={true} // TODO: implement this functionality then enable the button
-      >
-        Settings
-      </Button>
+      <SettingsButton gameId={gameId} boardId={boardId} teams={teams} />
     </Card>
   );
 };
